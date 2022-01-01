50acd49b7e3b763011e78be173849325d7586ea4
Stav Green <stavgn@gmail.com>
2022-01-01 17:29:08 +0200

    add queue node struct

--- queue.h
+++ queue.h
@@ -1,4 +1,5 @@
 #ifndef __QUEUE__
+#include <pthread.h>
 
 typedef struct Queue
 {
@@ -10,9 +11,15 @@ typedef struct Queue
 
 } queue_t;
 
+typedef struct QueueNode
+{
+    pthread_t thread_id;
+    int connfd;
+} qnode_t;
+
 queue_t *init(int queue_max_length);
-int enqueue(queue_t *q, int fd);
-int dequeue(queue_t *q);
+int enqueue(queue_t *q, qnode_t *node);
+qnode_t *dequeue(queue_t *q);
 int count_free_cells(queue_t *q);
 void destroy(queue_t *q);
 
