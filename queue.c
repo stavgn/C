638d25afda2c50501da7dfde7a6c811aa6a497e8
Dor <dor.shacham@gmail.com>
2022-01-01 17:54:34 +0200

    queue update

--- queue.c
+++ queue.c
@@ -1,9 +1,64 @@
-
-
 #include "queue.h"
 
-queue_t *init(int queue_max_length);
-int enqueue(queue_t *q, int fd);
-int dequeue(queue_t *q);
-int count_free_cells(queue_t *q);
-void destroy(queue_t *q);
+queue_t *init(int queue_max_length)
+{
+    queue_t *this = malloc(sizeof(queue_t));
+    if (this == NULL)
+    {
+        return this;
+    }
+    this->queue_max_length = queue_max_length;
+    this->length = 0;
+    this->next_cell = 0;
+    this->oldest_record = -1;
+    qnode_t *array = malloc(sizeof(qnode_t)*queue_max_length);
+    if (array == NULL)
+    {
+        free(this);
+        return NULL;
+    }
+    this->queue = array;
+    return this;
+}
+
+void destroy(queue_t *q)
+{
+    free(q->queue);
+    free(q);
+}
+
+int enqueue(queue_t *q, qnode_t node)
+{
+    if(q->length == q->queue_max_length) // queue is full
+    {
+        return -1;
+    }
+    q->queue[q->next_cell] = node;
+    if(q->length == 0)
+    {
+        q->oldest_record = q->next_cell;
+    }
+    q->next_cell = (q->next_cell + 1) % q->queue_max_length;
+    q->length++;
+    return 0;
+}
+int dequeue(queue_t *q, qnode_t *node)
+{
+    if ((node == NULL) || (q->length == 0))
+    {
+        return -1;
+    }
+    q->length--;
+    *node = q->queue[q->oldest_record];
+    q->oldest_record = (q->oldest_record + 1) % q->queue_max_length;
+    if (q->length == 0)
+    {
+        q->oldest_record = -1;
+    }
+    return 0;
+}
+
+int count_free_cells(queue_t *q)
+{
+    return q->queue_max_length - q->length;
+}
--- queue.h
+++ queue.h
@@ -1,15 +1,7 @@
 #ifndef __QUEUE__
 #include <pthread.h>
 
-typedef struct Queue
-{
-    int queue_max_length;
-    int length;
-    int next_cell;
-    int oldest_record;
-    int **queue;
 
-} queue_t;
 
 typedef struct QueueNode
 {
@@ -17,9 +9,19 @@ typedef struct QueueNode
     int connfd;
 } qnode_t;
 
+typedef struct Queue
+{
+    int queue_max_length;
+    int length;
+    int next_cell;
+    int oldest_record;
+    qnode_t *queue;
+
+} queue_t;
+
 queue_t *init(int queue_max_length);
-int enqueue(queue_t *q, qnode_t *node);
-qnode_t *dequeue(queue_t *q);
+int enqueue(queue_t *q, qnode_t node);
+int dequeue(queue_t *q, qnode_t *node);
 int count_free_cells(queue_t *q);
 void destroy(queue_t *q);
 
